dd9262c6222878871716deb0c02db8cd4a5d1e6a
Damian Krawczyk <pirrat82@gmail.com>
2026-06-25 17:54:00 +0200

    fix: remove unused React imports to satisfy TypeScript compiler

--- src/pages/InstructorBookingModal/InstructorBookingModal.tsx
+++ src/pages/InstructorBookingModal/InstructorBookingModal.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './InstructorBookingModal.module.scss';
 import { BookingCalendar } from '../BookingCalendar/BookingCalendar';
 import { useTranslation } from 'react-i18next';
