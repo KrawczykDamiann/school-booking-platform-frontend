b90262d423f3e3e54c7dbb6bd11eeb8fb0129e66
Damian Krawczyk <pirrat82@gmail.com>
2026-07-03 16:52:59 +0200

    fix: resolve typescript import error

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './LoginPage.module.scss';
-import { RequestMagicLinkForm } from '../../features/auth/RequestMagicLinkForm';
-
+import RequestMagicLinkForm from "../../features/auth/RequestMagicLinkForm";
 export const LoginPage: React.FC = () => {
   return (
     <div className={styles.loginPageWrapper}>
       {/* Target glassmorphic scope using the modules object properties */}
       <div className={styles.loginCard}>
         {/* Rendering the modular magic link form inside the styled glass viewport */}
-        <RequestMagicLinkForm />
+        <RequestMagicLinkForm onClose={function (): void {
+          throw new Error('Function not implemented.');
+        } } />
       </div>
     </div>
   );
 };
+
